6b04d8c999aeca6201e9a110224b2e5b26d514bd
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-01-22 16:19:50 +0200

    Update ifsend.sh

--- ifsend.sh
+++ ifsend.sh
@@ -1,4 +1,6 @@
 printf "SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 0 0 * * MON root /bin/bash /root/send.sh > /dev/null 2>&1
+0 0 * * MON root /bin/bash /root/mint.sh > /dev/null 2>&1
+0 0 * * MON root /bin/bash /root/burn.sh > /dev/null 2>&1
 " > /etc/cron.d/sendweek
