153e00c4c2b904498bae71532dd22c8eabb0a35e
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-01-22 16:20:45 +0200

    Update and rename send.sh to mint.sh

--- mint.sh
+++ mint.sh
@@ -1,3 +1,3 @@
-sudo tee /root/send.sh > /dev/null <<EOF
+sudo tee /root/mint.sh > /dev/null <<EOF
 echo y | ironfish wallet:mint -a 0.1 -f "$IRON" -m $IRONFISH_WALLET -n $IRONFISH_WALLET -o 0.00000001 | tee -a /root/logfile.log
 EOF
