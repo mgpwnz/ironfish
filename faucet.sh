fd9388e00b11944ab7ce9302a71a45915ff87dee
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-01-23 12:38:17 +0200

    Update faucet.sh

--- faucet.sh
+++ faucet.sh
@@ -1,18 +1,2 @@
 #!/bin/sh
-#v 0.1
-if [ ! $MAIL ]; then
-		read -p "Enter mail address: " MAIL
-		echo 'export MAIL='${MAIL} >> $HOME/.bash_profile
-	fi
-	. $HOME/.bash_profile
-  echo -e '\n\e[42mYour mail address:' $MAIL '\e[0m\n'
 echo $MAIL | ironfish faucet | tee -a /root/logfile.log
-
-
-
-#########################################################################
-#printf "SHELL=/bin/bash                                                #
-#PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin      #
-#00 11 * * * root /bin/bash /root/faucet.sh > /dev/null 2>&1            #
-#" > /etc/cron.d/afish                                                  #
-#########################################################################
